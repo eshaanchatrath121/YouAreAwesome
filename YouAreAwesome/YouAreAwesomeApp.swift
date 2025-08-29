e051092cfde0fa56a4afd183e6e70d2b361c6733
eshaanchatrath121 <eshaanchatrath121@gmail.com>
2025-08-29 13:09:23 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Eshaan Chatrath on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
